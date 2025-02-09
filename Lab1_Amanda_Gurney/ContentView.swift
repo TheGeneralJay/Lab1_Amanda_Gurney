de60ccd519943464d38a80cabf5f63b7b9c06f7e
Jay <amandagurns@hotmail.ca>
2025-02-09 11:36:48 -0500

    added prime numbers variable

--- Lab1_Amanda_Gurney/ContentView.swift
+++ Lab1_Amanda_Gurney/ContentView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    private var primeNumbers = [
+                                 2, 3, 5, 7, 11,
+                                 13, 17, 19, 23, 29,
+                                 31, 37, 41, 43, 47,
+                                 53, 59, 61, 67, 71,
+                                 73, 79, 83, 89, 97
+    ]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
