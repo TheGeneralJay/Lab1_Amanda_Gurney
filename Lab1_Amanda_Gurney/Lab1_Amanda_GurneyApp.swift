cb1c8489a14a5293ffb15e342d9d9a1f7516c258
Jay <amandagurns@hotmail.ca>
2025-02-09 10:56:28 -0500

    Initial Commit

---
+++ Lab1_Amanda_Gurney/Lab1_Amanda_GurneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Amanda_GurneyApp.swift
+//  Lab1_Amanda_Gurney
+//
+//  Created by Amanda Gurney on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Amanda_GurneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
